adceeb3d969fa7acc9647c9b46f03e5e8d855656
unknown <alvesmlktop@gmail.com>
2024-01-11 11:18:56 -0300

    service criado

---
+++ src/main/java/com/victoralves/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,22 @@
+package com.victoralves.algafood.di.service;
+
+import com.victoralves.algafood.di.modelo.Cliente;
+import com.victoralves.algafood.di.notificacao.Notificador;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+
+        System.out.println("AtivacaoClienteService: " + notificador);
+    }
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+}
