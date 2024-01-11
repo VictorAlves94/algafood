fd5f59bb3fa33cc04dd05fcbbe77045672f2860d
unknown <alvesmlktop@gmail.com>
2024-01-11 11:21:46 -0300

    dependencia injetada no controller

--- src/main/java/com/victoralves/algafood/Controller1.java
+++ src/main/java/com/victoralves/algafood/Controller1.java
@@ -1,14 +1,24 @@
 package com.victoralves.algafood;
 
+import com.victoralves.algafood.di.modelo.Cliente;
+import com.victoralves.algafood.di.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Controller1 {
+    private AtivacaoClienteService ativacaoClienteService;
+
+    public Controller1(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+    }
 
     @GetMapping("/hello")
-        @ResponseBody
+    @ResponseBody
     public String hello() {
+        Cliente joao = new Cliente("João", "joao@xyz.com", "3499998888");
+
+        ativacaoClienteService.ativar(joao);
         return "olá";
     }
 }
