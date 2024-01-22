029c8a1412daea404531913bf38a811225add791
unknown <alvesmlktop@gmail.com>
2024-01-22 20:30:32 -0300

    classe para testes nao web criada

---
+++ src/main/java/com/victoralves/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,21 @@
+package com.victoralves.algafood.jpa;
+
+import com.victoralves.algafood.domain.model.Cozinha;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Cozinha> listar(){
+     TypedQuery<Cozinha> query = manager.createQuery("from Cozinha",Cozinha.class);
+
+    return query.getResultList();
+    }
+}
