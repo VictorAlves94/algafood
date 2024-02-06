9a21bd9f2114091925fe4ba8dbc8704851e5ee8e
unknown <alvesmlktop@gmail.com>
2024-02-06 10:11:14 -0300

    endpoint buscar por id incluido.

--- src/main/java/com/victoralves/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/victoralves/algafood/api/controllers/CozinhaController.java
@@ -4,10 +4,9 @@ import com.victoralves.algafood.domain.model.Cozinha;
 import com.victoralves.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +19,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
     return cozinhaRepository.listar();
+    }
 
-
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id){
+        return cozinhaRepository.buscar(id);
     }
 }
