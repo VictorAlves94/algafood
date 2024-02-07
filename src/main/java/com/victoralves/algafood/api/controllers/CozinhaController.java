9467d1d1de8afd3a7f459e4c360a7173dff91a53
unknown <alvesmlktop@gmail.com>
2024-02-07 15:33:37 -0300

    criado service para cozinha

--- src/main/java/com/victoralves/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/victoralves/algafood/api/controllers/CozinhaController.java
@@ -1,22 +1,29 @@
 package com.victoralves.algafood.api.controllers;
 
+import com.ctc.wstx.shaded.msv_core.util.Uri;
 import com.victoralves.algafood.domain.model.Cozinha;
 import com.victoralves.algafood.domain.model.CozinhasXmlWrapper;
 import com.victoralves.algafood.domain.repository.CozinhaRepository;
+import com.victoralves.algafood.service.CozinhaService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(value = "/cozinhas" )//, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private CozinhaService cozinhaService;
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar(){
     return cozinhaRepository.listar();
@@ -27,8 +34,83 @@ public class CozinhaController {
         return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable Long id){
-        return cozinhaRepository.buscar(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
+
     }
-}
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+     return cozinhaService.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinha_id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinha_id ,@RequestBody Cozinha cozinha){
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinha_id);
+
+        if (cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual,"id");
+            cozinhaService.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{cozinha_id}")
+    public ResponseEntity<Cozinha> delete(@PathVariable Long cozinha_id){
+        try{
+            Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinha_id);
+
+            if (cozinhaAtual != null) {
+                cozinhaRepository.remover(cozinhaAtual);
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        }catch (DataIntegrityViolationException ex){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       //return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+//
+//
+
+//       return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//        return ResponseEntity.ok(cozinha);
+//        return ResponseEntity.status(HttpStatus.FOUND)
+//                .headers(headers)
+//                .build();
+
